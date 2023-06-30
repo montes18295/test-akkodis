452f2766be17ece4b54f0d3855872d8581aa65e2
Jose <montes18295@gmail.com>
2023-06-30 13:55:58 +0200

    Se configura integration test de controller a falta de desarrollarlo

--- src/test/java/org/akkodis/test/application/PriceControllerTest.java
+++ src/test/java/org/akkodis/test/application/PriceControllerTest.java
@@ -1,4 +1,36 @@
 package org.akkodis.test.application;
 
+import org.akkodis.test.Application;
+import org.akkodis.test.domain.service.PriceService;
+import org.junit.Test;;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = Application.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application.yml")
 public class PriceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getPriceByFilter() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/price/filter"))
+                .andDo(print());
+    }
+
+
 }
